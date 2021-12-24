25a1a7d7bc22a8d9aaa958c363108c8d80173c3f
Umair Jibran <umairjibran7@gmail.com>
2021-12-24 20:20:58 +0500

    remove dummy

--- controllers/foo.js
+++
@@ -1,7 +0,0 @@
-function foo({ message }) {
-	return async (req, res) => {
-		res.status(200).json({ success: true, message });
-	};
-}
-
-module.exports = { foo };
--- routes/foo.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-// Controllers
-const { createFoo } = require("../controllers");
-
-router.get("/", createFoo);
-
-module.exports = router;
