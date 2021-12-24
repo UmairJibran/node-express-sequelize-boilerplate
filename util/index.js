c124ee60fe8350aefb5a7601a29a1b9f07d12321
Umair Jibran <umairjibran7@gmail.com>
2021-12-24 20:20:36 +0500

    Update index.js

--- util/index.js
+++ util/index.js
@@ -1,7 +1,8 @@
 const _ = require("lodash");
 
-const isTrue = value => value === true || value.toLowerCase() === "true";
-const isEmpty = value => _.isEmpty(value);
+const isTrue = (value) => value === true || value.toLowerCase() === "true";
+const isEmpty = (value) => _.isEmpty(value);
+const isNil = (value) => _.isNil(value);
 
 Object.freeze(isTrue, isEmpty);
-module.exports = { isTrue, isEmpty };
+module.exports = { isTrue, isEmpty, isNil };
