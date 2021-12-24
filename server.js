8f577850db8cb488a3e0cdacde8cba837298476b
Umair Jibran <umairjibran7@gmail.com>
2021-12-24 20:33:48 +0500

    prttier

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require("./app");
 const { PORT } = process.env;
 
 app.listen(PORT, () => {
-	console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`);
 });
