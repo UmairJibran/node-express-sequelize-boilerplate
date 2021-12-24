3cd5fd65be4b3fdef622fe0cf8114b5e11e6048a
Umair Jibran <umairjibran7@gmail.com>
2021-12-24 20:34:05 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const router = require("./routes");
 
 const app = express();
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use("/api", router);
 
 module.exports = app;
