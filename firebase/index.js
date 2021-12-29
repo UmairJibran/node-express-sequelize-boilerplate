a42143a8fec86ea4743f8fd65d4c949e62ecbabc
Umair Jibran <umairjibran7@gmail.com>
2021-12-29 21:30:15 +0500

    skip firebase init for `test_remote`

--- firebase/index.js
+++ firebase/index.js
@@ -4,13 +4,20 @@ const config = require("../config");
 const firebase = { auth: {} };
 
 // credential: admin.credential.cert(credentials.get("firebase"))
-const defaultApp = admin.initializeApp({
-  credential: admin.credential.cert({
-    project_id: config.get("firebase").project_id,
-    private_key: config.get("firebase").private_key.replace(/\\n/g, "\n"),
-    client_email: config.get("firebase").client_email,
-  }),
-});
+const defaultApp =
+  config.get("env") === "test_remote"
+    ? {
+        auth: () => {
+          console.log("temp stub");
+        },
+      }
+    : admin.initializeApp({
+        credential: admin.credential.cert({
+          project_id: config.get("firebase").project_id,
+          private_key: config.get("firebase").private_key.replace(/\\n/g, "\n"),
+          client_email: config.get("firebase").client_email,
+        }),
+      });
 
 const auth = defaultApp.auth();
 
